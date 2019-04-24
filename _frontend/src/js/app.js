c1e65ce6f5a277057d2ba50ad9b65999db1700db
Simon Arnold <arnolali@gmail.com>
2019-04-24 18:04:02 -0400

    - update.

--- _frontend/src/js/app.js
+++ _frontend/src/js/app.js
@@ -3,9 +3,9 @@ import { throttle, debounce } from "throttle-debounce";
 class Site {
   constructor() {
     this.dom = {
-      header: document.querySelector('.site-header'),
-      main: document.querySelector('main'),
-      footer: document.querySelector('.site-footer')
+      header: document.querySelector(".site-header"),
+      main: document.querySelector("main"),
+      footer: document.querySelector(".site-footer")
     };
 
     this.previousScrollY = 0;
@@ -31,8 +31,14 @@ class Site {
     let scrollY = window.pageYOffset || document.documentElement.scrollTop;
     let isScrollingDown = scrollY > this.previousScrollY ? true : false;
 
-    document.documentElement.classList.toggle('is-scrolling-down', isScrollingDown);
-    document.documentElement.classList.toggle('is-scrolling-up', !isScrollingDown);
+    document.documentElement.classList.toggle(
+      "is-scrolling-down",
+      isScrollingDown
+    );
+    document.documentElement.classList.toggle(
+      "is-scrolling-up",
+      !isScrollingDown
+    );
     this.previousScrollY = scrollY <= 0 ? 0 : scrollY;
   }
 
